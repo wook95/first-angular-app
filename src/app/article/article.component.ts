096e98d311e9a81efa823aa56e1c69e099114a9d
wook95 <uclee95@gmail.com>
2022-04-11 21:58:42 +0900

    feat: 투표수를 변경할 수 있는 article 컴포넌트 구현

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,31 @@
+import { Component, HostBinding, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title = 'Angular';
+    this.link = 'https://angular.kr/';
+    this.votes = 10;
+  }
+
+  voteUp(): boolean {
+    this.votes += 1;
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.votes -= 1;
+    return false;
+  }
+
+  ngOnInit() {}
+}
