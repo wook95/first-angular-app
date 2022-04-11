877ca74610cbf730323623e24fdf38754f6fa3a0
wook95 <uclee95@gmail.com>
2022-04-11 23:12:08 +0900

    feat: submit버튼을 누르면 실제로 삽입되고,정렬되도록 구현

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,12 @@ export class AppComponent {
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
+  }
+  sortArticle(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
   }
 }
