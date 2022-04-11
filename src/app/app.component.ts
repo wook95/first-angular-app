487f49296bb0186bf75bfc43d9a8e2948356c5c3
wook95 <uclee95@gmail.com>
2022-04-11 22:58:04 +0900

    feat: 배열을 받아, 여러개의 아티클을 렌더링

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'https://angular.io', 3),
+      new Article('wooks blog', 'http://wook95.github.io', 2),
+      new Article('Angular Korean Homepage', 'https://angular.kr/', 1),
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
